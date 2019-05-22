370eddd9f0aa3c98ec619162791aa4a6ca3f23bf
Alex Kolarski <aleks.rk@gmail.com>
2019-05-22 12:41:33 +0300

    Added App unit test

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import App from './App'
+import renderer from 'react-test-renderer'
+
+it('renders correctly', () => {
+  const tree = renderer.create(<App />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
